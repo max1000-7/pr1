a8ebbf08e19ad94ae2ac2a7dcf1a284cae36fb4f
LaS <LaS@mail.com>
2024-01-24 12:05:00 +0300

    Практическая работа 1

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Hello
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("x= ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("y= ");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine((Math.Sin(x)+ Math.Cos(y)/ Math.Cos(x) + Math.Sin(y))*Math.Tan(x*y));
+      }
+    }
+}
